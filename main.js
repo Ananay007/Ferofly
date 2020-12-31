f446488399cab2c2f248bdf647100e19790e0a87
Ananay007 <43534256+Ananay007@users.noreply.github.com>
2020-12-31 15:46:33 +0530

    Google Maps included in code
    Google maps key expired so it will not show in browser but otherwise it is present in code.
    Webpage might not look the same because of the different pictures and font family used.

---
+++ main.js
@@ -0,0 +1,38 @@
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+
+  // Smooth Scrolling
+$('html,body').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top -100
+        },
+    3000
+      );
+    }
+  });
+  
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+  });  
